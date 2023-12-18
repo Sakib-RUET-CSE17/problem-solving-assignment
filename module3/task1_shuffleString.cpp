ed005f78772661563304c1a3e3d4da0c02e52837
Sakib Hasan <hsakib8685@gmail.com>
2023-12-18 10:51:30 +0600

    shuffleString added

---
+++ module3/task1_shuffleString.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define db double
+#define pb push_back
+#define pi acos(-1.0)
+#define mod 1000000007
+
+string shuffleString(string s, vector<int> &indices) {
+  string ans = s;
+  for (int i = 0; i < indices.size(); i++) {
+    ans[indices[i]] = s[i];
+  }
+  return ans;
+}
+
+int main() {
+  string s = "mamacode";
+  vector<int> indices{4, 5, 6, 7, 0, 1, 2, 3};
+  cout << shuffleString(s, indices) << endl;
+  return 0;
+}
